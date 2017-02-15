2f0a793b598869dfd31e0f9a0fd2d54b54f6a608
AdamB37 <adam.beshir@gmail.com>
2017-02-15 00:08:40 -0800

    completed

--- excercises/guess_the_number.js
+++ excercises/guess_the_number.js
@@ -1,30 +1,20 @@
-var prompt = require('prompt')
-
-function guess_the_number() {
-  let number = Math.floor(Math.random()*100+1)
-
-  prompt.start()
-
-  function guess() {
-    prompt.get(['guess'], (err,result) => {
-
-      if(result.guess > number) {
-        console.log("Too high homie, again")
-        return guess()
-      }
-
-      if(result.guess < number) {
-        console.log("Too low bro, guess again")
-        return guess()
-      }
-
-      console.log("you win!")
-      prompt.stop()
-    })
-
+export default function guessTheNumber(input) {
+  let number =  81 //Math.floor(Math.random()*100)+1 (turned off random for testing)
+  if(input > number) {
+    console.log("Too high")
+    return "Too high"
   }
-  guess()
+  if(input < number) {
+    console.log("Too low")
+    return "Too low"
+  }
+  if(input === "quit") {
+    return
+  }
 
+  if(typeof(input)!=='number'){
+    console.log("please enter an integer")
+  }
+  console.log("You win")
+  return "You win"
 }
-
-guess_the_number()
