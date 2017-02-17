275d6d942667fe0a80bf327e338c02e9b8e95d8b
AdamB37 <adam.beshir@gmail.com>
2017-02-17 10:17:44 -0800

    wip

--- excercises/where_in_the_json.js
+++ excercises/where_in_the_json.js
@@ -1,21 +1,3 @@
 export default function whereInTheJson(json, value) {
-  var obj = JSON.parse(json)
-  let keys = Object.keys(obj)
-  console.log(keys)
-  return matchInObject(obj, keys, value)
-}
 
-function matchInObject(obj, keys, value, path = '') {
-  console.log('keys', keys)
-  let key = keys.pop()
-  if(obj[key] === value) {
-    console.log(path + key)
-    return path + key
-  }
-  else if(typeof(obj[key]) === 'object') {
-    return matchInObject(obj[key], Object.keys(obj[key]), value, path + key + ' -> ')
-  }
-  else if(keys.length) {
-    return matchInObject(obj, keys, value)
-  }
 }
